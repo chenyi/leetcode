906c12985463a3efa65ef6b2850613ec04ad54d8
陈懿 <chengyi.cy@alibaba-inc.com>
2020-12-04 23:28:28 +0800

    commit

--- 21.合并两个有序链表.java
+++ 21.合并两个有序链表.java
@@ -17,7 +17,28 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        
+       ListNode tail = new ListNode(0);
+       ListNode head = new ListNode(0, tail);
+
+       while(l1 != null && l2 != null){
+         int v1 = l1.val;
+         int v2 = l2.val;
+         if(v1 < v2){
+            tail.next = l1;
+            l1 = l1.next;
+         }else{
+            tail.next = l2;
+            l2 = l2.next;
+         }
+         tail = tail.next;
+       }
+       if(l1 == null){
+           tail.next = l2;
+       }else{
+           tail.next = l1;
+       }
+
+       return head.next.next;
     }
 }
 // @lc code=end
