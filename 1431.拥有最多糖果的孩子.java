8c9a5b1301e1a8bf8c85532f698d2e4a52b39a95
陈懿 <chengyi.cy@alibaba-inc.com>
2020-12-05 19:17:57 +0800

    main

--- 1431.拥有最多糖果的孩子.java
+++ 1431.拥有最多糖果的孩子.java
@@ -7,7 +7,20 @@
 // @lc code=start
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-
+        int max = 0;
+        for(int i = 0; i < candies.length; i++){
+            max = Math.max(max, candies[i]);
+        }
+        List<Boolean> list = new ArrayList<Boolean>();
+        for(int i = 0; i < candies.length; i++){
+            int couldBe = candies[i] + extraCandies;
+            if(couldBe >= max){
+                list.add(true);
+            }else{
+                list.add(false);
+            }
+        }        
+        return list;
     }
 }
 // @lc code=end
