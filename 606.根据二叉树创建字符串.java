5c8fd2b8644526611c7bd5f66af0c119d63891de
陈懿 <chengyi.cy@alibaba-inc.com>
2020-12-08 12:14:58 +0800

    main

--- 606.根据二叉树创建字符串.java
+++ 606.根据二叉树创建字符串.java
@@ -16,27 +16,42 @@
  */
 class Solution {
     public String tree2str(TreeNode t) {
+        // if(t == null){
+        //     return "";
+        // }
+        // StringBuffer sb = new StringBuffer();
+        // sb.append(t.val);
+        // if(t.left != null || t.right != null){
+        //   if(t.left != null){
+        //       sb.append("(");
+        //       sb.append(tree2str(t.left));
+        //       sb.append(")");
+        //     }else{
+        //            sb.append("()");
+        //     }
+
+        //   if(t.right != null){
+        //     sb.append("(");
+        //     sb.append(tree2str(t.right));
+        //     sb.append(")");
+        //   }
+        // }
+        // return sb.toString();
+
         if(t == null){
             return "";
         }
         StringBuffer sb = new StringBuffer();
         sb.append(t.val);
-        if(t.left != null || t.right != null){
-          if(t.left != null){
-              sb.append("(");
-              sb.append(tree2str(t.left));
-              sb.append(")");
-            }else{
-                sb.append("()");
-            }
-
-        if(t.right != null){
-            sb.append("(");
-            sb.append(tree2str(t.right));
-            sb.append(")");
+        String leftText = tree2str(t.left);
+        String rigthText = tree2str(t.right);
+        if(t.left == null && t.right == null){
+            return sb.toString();
         }
-    }
-        return sb.toString();
+        if(t.left != null && t.right == null){
+            return sb.append("(").append(leftText).append(")").toString();
+        }
+        return sb.append("(").append(leftText).append(")").append("(").append(rigthText).append(")").toString();
     }
 }
 // @lc code=end
