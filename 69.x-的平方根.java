cda0f0f77a2ba85f14646d93b69081a23aa17072
陈懿 <chengyi.cy@alibaba-inc.com>
2020-12-11 20:56:11 +0800

    mIN

---
+++ 69.x-的平方根.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=java
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1){
+            return x;
+        }
+        int l = 0;
+        int r = x/2 + 1;
+        while(l < r){
+            int mid = l + (r - l) /2;
+            if(mid > x / mid){
+                r = mid;
+            }else{
+                l = mid + 1;
+            }       
+        }
+        return l - 1;
+    }
+}
+// @lc code=end
+
