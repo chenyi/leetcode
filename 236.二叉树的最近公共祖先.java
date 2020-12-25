3540ddb92f18b64751bdb787bca6fe555ab89039
陈懿 <chengyi.cy@alibaba-inc.com>
2020-12-26 03:00:23 +0800

    main

--- 236.二叉树的最近公共祖先.java
+++ 236.二叉树的最近公共祖先.java
@@ -15,9 +15,49 @@
  * }
  */
 class Solution {
+
+    TreeNode ans;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+       Map<Integer, TreeNode> map = new HashMap<Integer, TreeNode>(); 
+       dfs(root, map);
+
+        TreeNode pp = p;
+        Set<TreeNode> paths = new HashSet<TreeNode>();
+        while(pp != null){
+           paths.add(pp);
+           pp = map.get(pp.val);
+        }
+
+        TreeNode qp = q;
+        while(qp != null){
+            if(paths.contains(qp)){
+                break;
+            }
+            qp = map.get(qp.val);
+        }
+        return qp;
+
     }
+
+    public void dfs(TreeNode root, Map<Integer, TreeNode> map){
+        if(root == null){
+            return;
+        }
+        TreeNode l = root.left;
+        TreeNode r = root.right;
+
+        if(l != null){
+            map.put(l.val, root);
+        }
+        if(r != null){
+            map.put(r.val, root);
+        }
+        dfs(root.left, map);
+        dfs(root.right, map);
+
+    }
+
 }
 // @lc code=end
 
